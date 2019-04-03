1569874f263c31d6b63e9a22640dad4fdda136c4
LisaTheWolpertinger <49234377+LisaTheWolpertinger@users.noreply.github.com>
2019-04-03 12:48:15 +0200

    Update numbergame.py

--- numbergame.py
+++ numbergame.py
@@ -3,14 +3,14 @@ import random
 randomnumber = random.randint(1,100)
 
 print("Welcome to the great number game! Let's see, if you can guess the number I am thinking about.")
-tip = int(input("Tip a number between 1 and 100: "))
+tip = int(input("Guess a number between 1 and 100: "))
 
 while tip != randomnumber:
     if tip > randomnumber:
-        print("The Number you are looking for must be smaller. Try again! Your last tip:",tip)
-        tip = int(input("Your next tip: "))
+        print("The Number I am looking for must be smaller. Try again! Your last guess:",tip)
+        tip = int(input("Your next guess: "))
     elif tip < randomnumber:
-        print("The Number you are looking for must be larger. Try again! Your last tip:", tip)
-        tip = int(input("Your next tip: "))
+        print("The Number you are looking for must be larger. Try again! Your last guess:", tip)
+        tip = int(input("Your next guess: "))
 
 print("Yeah, That's right! It was",randomnumber, "! Congrats!" )
